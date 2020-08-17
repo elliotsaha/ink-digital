63a158fd30665a091e1d113510b084aaae1fbd89
HerilS <herilsaha06@gmail.com>
2020-08-16 20:20:52 -0700

    Made First Panel For Case Study pages

---
+++ src/components/clients/rajahmaggay/PanelOne.js
@@ -0,0 +1,114 @@
+import React from "react"
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles"
+import rajahMaggay from "../../../images/rajahMaggay.png"
+const useStyles = makeStyles(theme =>
+  createStyles({
+    root: {
+      backgroundColor: "#262628",
+      paddingTop: "5rem",
+      overflow: 'hidden',
+    },
+    mainTitle: {
+      fontFamily: "Poppins, sans-serif",
+      fontWeight: "bolder",
+      color: "white",
+      fontSize: "5rem",
+      lineHeight: "6.5rem",
+      paddingBottom: "2rem",
+      [theme.breakpoints.down(970)]: {
+        fontSize: "3rem",
+        lineHeight: "4rem",
+      },
+      [theme.breakpoints.down(750)]: {
+        fontSize: "2.3rem",
+        lineHeight: "3rem",
+      },
+    },
+    image: {
+      width: "70%",
+      objectFit: "cover",
+      margin: 0,
+      [theme.breakpoints.down(750)]: {
+        width: '100%',
+      },
+    },
+    imageContainer: {
+      backgroundColor: "#3C3C41",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+    text: {
+      fontFamily: "Poppins, sans-serif",
+      color: "white",
+      paddingLeft: "2rem",
+      paddingRight: "2rem",
+      paddingBottom: "2rem",
+      [theme.breakpoints.down(981)]: {
+        paddingLeft: "1rem",
+      },
+    },
+    subTitles: {
+      paddingBottom: "0.5rem",
+      textTransform: "uppercase",
+      fontSize: "1.2rem",
+      fontWeight: 600,
+    },
+    para: {
+      maxWidth: "60rem",
+      paddingBottom: "2rem",
+      [theme.breakpoints.down(750)]: {
+        fontSize: "0.9rem",
+      },
+    },
+    textContainer: {
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+    },
+  })
+)
+export default function PanelOne() {
+  const classes = useStyles()
+  return (
+    <div className={classes.root}>
+      <div className={classes.textContainer}>
+        <div className={classes.text}>
+          <div className={classes.mainTitle}>
+            Rajah Maggay: <br />
+            Political Campaign.
+          </div>
+          <div>
+            <div className={classes.subTitles}>Who Are They</div>
+            <div className={classes.para}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              tempus, dolor et semper malesuada, velit ex imperdiet ante, at
+              fringilla ipsum arcu sed dui. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Sed tempus, dolor et semper
+              malesuada, velit ex imperdiet ante, at fringilla ipsum arcu sed
+              dui.
+            </div>
+          </div>
+          <div>
+            <div className={classes.subTitles}>The Struggle</div>
+            <div className={classes.para}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              tempus, dolor et semper malesuada, velit ex imperdiet ante, at
+              fringilla ipsum arcu sed dui. Lorem ipsum dolor sit amet,
+              consectetur adipiscing elit. Sed tempus, dolor et semper
+              malesuada, velit ex imperdiet ante, at fringilla ipsum arcu sed
+              dui. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+              tempus, dolor et semper malesuada, velit ex imperdiet ante, at
+              fringilla ipsum arcu sed dui.
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={classes.imageContainer}>
+        <img src={rajahMaggay} className={classes.image} />
+      </div>
+    </div>
+  )
+}
---
+++ src/pages/rajahmaggay.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import PanelOne from '../components/clients/rajahmaggay/PanelOne'
+import Layout from "../components/layout"
+export default function rajahmaggay() {
+    return (
+        <Layout>
+            <PanelOne />
+        </Layout>
+    )
+}
